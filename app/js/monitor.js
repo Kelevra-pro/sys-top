549547f18602a972b869bb58615224d919bf4623
kelevra-pro <damtat8@gmail.com>
2021-12-19 18:08:45 +0300

    use settings data in monitor

--- app/js/monitor.js
+++ app/js/monitor.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const { ipcRenderer } = require('electron');
 const osu = require('node-os-utils');
 const cpu = osu.cpu;
 const mem = osu.mem;
@@ -6,6 +7,12 @@ const os = osu.os;
 
 let cpuOverload = 80;
 let alertFrequency = 5;
+
+// Get settings & values
+ipcRenderer.on('settings:get', (e, settings) => {
+  cpuOverload = +settings.cpuOverload;
+  alertFrequency = +settings.alertFrequency;
+});
 
 // Run every 2 seconds
 setInterval(() => {
